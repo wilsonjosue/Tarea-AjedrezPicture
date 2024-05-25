4cd04b5e7bf9ec20ca2cadf5e2f8034528c30c74
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-25 15:38:22 -0500

    Se empieza a resolver el Ejercicio2c.py usando un bucle for intento 1

--- Ejercicio2d.py
+++ Ejercicio2d.py
@@ -1,2 +1,19 @@
 from interpreter import draw
 from chessPictures import *
+from chessPictures import square
+
+"""Crear las imágenes necesarias."""
+black_square = square # Imagen vasia negra
+white_square = square.negative() # Imagen vasia blanca. 
+
+"""Crea la matriz row vacia"""
+row=[]
+""" Se realizara un bucle for pra llenar la matriz luego imprimirla"""
+for i in range(8):
+    if i%2== 0:
+        row.append(white_square) 
+    else:
+        row.append(black_square)
+
+# Dibujar
+draw(row)

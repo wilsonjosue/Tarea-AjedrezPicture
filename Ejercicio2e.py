3077592025d040d78477e3def148090b65d5461a
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-25 16:17:00 -0500

    Se resuelve el Ejercicio2e.py usando un bucle for para los tableros y el otro bucle para poder juntarlos en una fila intento 1.

--- Ejercicio2e.py
+++ Ejercicio2e.py
@@ -1,2 +1,28 @@
 from interpreter import draw
 from chessPictures import *
+from chessPictures import square
+
+"""Crear las imágenes necesarias."""
+black_square = square # Imagen vacia negra.
+white_square = square.negative() # Imagen vacia blanca. 
+
+"""Crea la matriz row vacia"""
+row=[]
+n=8
+
+""" Se realizara un bucle for para llenar la matriz empezando con el casillero negro"""
+for i in range(n):
+    if i%2== 0:
+        row.append(black_square) 
+    else:
+        row.append(white_square)
+
+"""Forma de unir los datos del arreglo row"""
+row_picture = row[0]# Asignar a row_picture el primer elemento de row.
+
+"""join(self, p):Devuelve nueva figura poniendo la figura del argumento al lado derecho de la figura actual"""
+for i in range(1,len(row)):#Unir todas las imágenes en una fila.
+    row_picture=row_picture.join(row[i])#Los pone a su lado derecho.
+    
+# Dibujar
+draw(row_picture)

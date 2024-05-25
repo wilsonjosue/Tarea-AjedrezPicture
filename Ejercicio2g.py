e15326b60a7e3bb37ee72268025f694f68c90dcf
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-25 17:23:00 -0500

    Se probara una matriz copn los datos ingresados deacuerdo a la posicion del tablero Ejercicio2g.py

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -3,48 +3,36 @@ from chessPictures import *
 
 
 """Crear las imágenes necesarias."""
-pawn_black= pawn.negative() #peon negro
-pawn
-bishop_black
-bishop
-knight_black
-knight
-rock_black
-rock
-king_black
-king
-queen_black
-queen
+pawn_black = pawn.negative() #_black negro
+pawn_white = pawn
+bishop_black = bishop.negative()
+bishop_white = bishop
+knight_black = knight.negative()
+knight_white = knight
+rock_black = rock.negative()
+rock_white = rock
+king_black = king.negative()
+king_white = king
+queen_black = queen.negative()
+queen_white = queen
 
-black_square = square.negative() # Imagen vacia negra.
-white_square = square # Imagen vacia blanca. 
+"""Ingresamos los datos a la tabla para las piezas negras"""
+posicion_inicial =[
+    rock_black, knight_black, bishop_black, queen_black, king_black, bishop_black, knight_black, 
+    rock_black,pawn_black, pawn_black, pawn_black, pawn_black, pawn_black, pawn_black, pawn_black, 
+    pawn_black,
+]
 
-"""Crea la matriz row1 y row2 vacia"""
-row1 = []  # Primera fila: empieza con casilla blanca.
-row2 = []  # Segunda fila: empieza con casilla negra.
+# Organiza las imágenes de las piezas en el tablero
+tablero = posicion_inicial[0]  # Inicializa el tablero con la primera imagen de la lista
 
 n = 8  # Número de casillas en una fila.
 
-""" Se realizara un bucle for para llenar la matriz empezando con el casillero para cada tipo"""
-for i in range(n):
-    if i % 2 == 0:
-        row1.append(white_square)
-        row2.append(black_square)
-    else:
-        row1.append(black_square)
-        row2.append(white_square)
-
-"""Forma de unir los datos del arreglo row"""
-row1_picture = row1[0]# Asignar a row_picture el primer elemento de row1.
-row2_picture = row2[0]# Asignar a row_picture el primer elemento de row2.
-"""join(self, p):Devuelve nueva figura poniendo la figura del argumento al lado derecho de la figura actual"""
-
-for i in range(1,len(row1)):#Unir todas las imágenes en una fila.
-    row1_picture=row1_picture.join(row1[i])#Los pone a su lado derecho.
-    row2_picture=row2_picture.join(row2[i])#Los pone a su lado derecho.
-
+"""Une las imágenes horizontalmentelizara un bucle for para llenar la matriz empezando con el casillero para cada tipo"""
+for img in initial_positions[1:]:
+    tablero = tablero.join(img)  # Une las imágenes horizontalmente
 """Unir los dos con up para unir las filas de p.img encima de las filas de self.img""" 
-nueva_tabla=row2_picture.up(row1_picture).up(row2_picture).up(row1_picture)
+
 # Dibujar
-draw(nueva_tabla)
+draw(tablero)
 

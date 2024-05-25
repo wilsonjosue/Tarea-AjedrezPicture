64a5257ffa35b7437da64908d30148c9b5d17fa1
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-25 13:34:13 -0500

    Se  completa el  Ejercicio2a.py deacuerdo a lo pedido

--- Ejercicio2a.py
+++ Ejercicio2a.py
@@ -5,11 +5,12 @@ from chessPictures import knight
 # Crear las imágenes necesarias
 white_knight = knight # Imagen del caballo blanco 
 black_knight = knight.negative() # Imagen del caballo negro 
-# Posiciones de los caballos
+
+# Posiciones de los caballos Mij varuable
 M11 = white_knight # Caballo blanco en la esquina superior derecha
 M10 = black_knight # Caballo negro en la esquina inferior derecha
-M01 = white_knight # Caballo blanco en la esquina superior izquierda
-M00 = black_knight # Caballo negro en la esquina inferior izquierda
+M01 = black_knight # Caballo blanco en la esquina superior izquierda
+M00 = white_knight # Caballo negro en la esquina inferior izquierda
 
 # Unir las imágenes en una tabla 2x2
 first_row = M00.join(M01)  # Primera fila de la tabla

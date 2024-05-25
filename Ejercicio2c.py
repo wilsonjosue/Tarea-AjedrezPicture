ecae8569ed1b348235c6f80eb6b82d52fd7ca7a3
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-25 14:44:15 -0500

    Se resuleve la p´romera version Ejercicio2c.py usando horizontalRepeat(3)

--- Ejercicio2c.py
+++ Ejercicio2c.py
@@ -1,2 +1,15 @@
 from interpreter import draw
 from chessPictures import *
+from chessPictures import queen
+
+"""Crear las imágenes necesarias."""
+white_queen = queen # Imagen del la reyna blanca. 
+
+"""Posiciones de la reina blanca."""
+M00 = white_queen # reyna blanca blanco. 
+
+""" Devuelve una nueva figura repitiendo la actual al costado la cantidad que indique el valor de n """
+repetir_queen = white_queen.horizontalRepeat(3) # Primera fila de la tabla
+
+# Dibujar
+draw(repetir_queen)

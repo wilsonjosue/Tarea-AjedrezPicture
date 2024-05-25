5bf401b5be0981c2d3229f9139f448c4d70527f2
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-25 14:16:51 -0500

    Se resuleve el ejercicio Ejercicio2b.py cambiando la posicion de los caballos en mi variable Mij

--- Ejercicio2b.py
+++ Ejercicio2b.py
@@ -1,21 +1,20 @@
 from interpreter import draw
 from chessPictures import *
-from interpreter import draw
 from chessPictures import knight
 
 # Crear las imágenes necesarias.
-white_knight1 = knight # Imagen del caballo blanco. 
-black_knight1 = knight.negative() # Imagen del caballo negro. 
+white_knight = knight # Imagen del caballo blanco. 
+black_knight = knight.negative() # Imagen del caballo negro. 
 
-white_knight_mirrow = white_knight1.horizontalMirror() # Imagen del caballo blanco 
-black_knight1_mirrow = black_knight1.horizontalMirror() # Imagen del caballo negro 
+white_knight_mirrow = white_knight.horizontalMirror() # Imagen del caballo blanco 
+black_knight_mirrow = black_knight.horizontalMirror() # Imagen del caballo negro 
 
 # Posiciones de los caballos Mij variable.
-M01 = white_knight1 # Caballo blanco en la esquina superior derecha
-M00 = black_knight1 # Caballo negro en la esquina inferior derecha
+M00 = white_knight # Caballo blanco. 
+M01 = black_knight # Caballo negro.
 # Posiciones de los caballos Mij variable Devuelve el espejo horizontal de la imagen
-M10 = white_knight_mirrow # Caballo blanco en la esquina superior izquierda
-M11 = black_knight1_mirrow # Caballo negro en la esquina inferior izquierda
+M10 = black_knight_mirrow # Caballo blanco.
+M11 = white_knight_mirrow # Caballo negro.
 
 # Unir las imágenes en una tabla 2x2
 first_row = M00.join(M01)  # Primera fila de la tabla

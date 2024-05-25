7e1e8688c0a687b17567ccaf5a15dfd3332a3f68
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-25 16:06:15 -0500

    Se resuelve el Ejercicio2d.py usando un bucle for para los tabletros en blanco y el otro for  para ñadir uno al lado de otro intento 2 error en madar el anterior commit.

--- Ejercicio2d.py
+++ Ejercicio2d.py
@@ -3,8 +3,8 @@ from chessPictures import *
 from chessPictures import square
 
 """Crear las imágenes necesarias."""
-black_square = square # Imagen vasia negra
-white_square = square.negative() # Imagen vasia blanca. 
+black_square = square # Imagen vacia negra.
+white_square = square.negative() # Imagen vacia blanca. 
 
 """Crea la matriz row vacia"""
 row=[]
@@ -17,11 +17,11 @@ for i in range(n):
         row.append(black_square)
 
 """Forma de unir los datos del arreglo row"""
-row_picture = row[0]# Asignar a row_picture el primer elemento de row
+row_picture = row[0]# Asignar a row_picture el primer elemento de row.
 
 """join(self, p):Devuelve nueva figura poniendo la figura del argumento al lado derecho de la figura actual"""
-for i in range(1,len(row)):#Unir todas las imágenes en una fila
-    row_picture=row_picture.join(row[i])#Los pone asu lado derecho
+for i in range(1,len(row)):#Unir todas las imágenes en una fila.
+    row_picture=row_picture.join(row[i])#Los pone a su lado derecho.
 
 # Dibujar
 draw(row_picture)
